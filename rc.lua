b13bd7e130e0654b5b26e50ce80e402eb4e98b47
Aoibhinn Nic Aoidh <oibind@pm.me>
2020-05-25 17:09:11 +0100

    Add missing end.

--- rc.lua
+++ rc.lua
@@ -55,6 +55,7 @@ local function run_once(cmd_arr)
     for _, cmd in ipairs(cmd_arr) do
         awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
     end
+end
 -- }}}
 
 -- {{{ Variable definitions
