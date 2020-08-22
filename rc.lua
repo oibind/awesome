a048f4b5674f7d553d605d45368607d7da867a75
Aoibhinn Nic Aoidh <oibind@pm.me>
2020-08-22 14:00:14 +0100

    Programs are already started at the launch of X

--- rc.lua
+++ rc.lua
@@ -635,5 +635,3 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- autostart (abrasive)
 awful.util.spawn("nm-applet")
-awful.util.spawn("picom -b")
-awful.util.spawn("unclutter -root")
